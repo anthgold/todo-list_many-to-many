e956375924890bbd86113aee4c305234b82a002e
Anthony Gold <anthgold@gmail.com>
2016-05-04 13:59:11 -0700

    change / route back to Map

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,7 +13,7 @@ public class App {
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
+      Map<String, Object> model = new HashMap<String, Object>();
       model.put("categories", Category.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
