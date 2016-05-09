d14227298e03f2d86459f98eb5a64a572a23f278
Anthony Gold and Kyle Wolfson <kyle@>
2016-05-09 16:37:10 -0700

    pass category creation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,8 +13,21 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      // model.put("categories", Category.all());
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("categories", Category.all());
+      model.put("template", "templates/categories.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -53,6 +66,26 @@ public class App {
       model.put("template", "templates/category.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/add_tasks", (request, response) -> {
+      int taskId = Integer.parseInt(request.queryParams("task_id"));
+      int categoryId = Integer.parseInt(request.queryParams("category_id"));
+      Category category = Category.find(categoryId);
+      Task task = Task.find(taskId);
+      category.addTask(task);
+      response.redirect("/categories/" + categoryId);
+      return null;
+    });
+
+    post("/add_categories", (request, response) -> {
+      int taskId = Integer.parseInt(request.queryParams("task_id"));
+      int categoryId = Integer.parseInt(request.queryParams("category_id"));
+      Category category = Category.find(categoryId);
+      Task task = Task.find(taskId);
+      task.addCategory(category);
+      response.redirect("/tasks/" + taskId);
+      return null;
+    });
 
   }
 }
