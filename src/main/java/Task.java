0435299474c678a8ee3c02bbd44a83218b8a31b7
Anthony Gold and Kyle Wolfson <kyle@>
2016-05-09 14:18:07 -0700

    pass tests addCategory() and getCategories()

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 import org.sql2o.*;
 
 public class Task {
@@ -73,5 +74,37 @@ public class Task {
       .executeUpdate();
     }
   }
+
+  public void addCategory(Category category) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO tasks_categories (task_id, category_id) VALUES (:task_id, :category_id)";
+      con.createQuery(sql)
+        .addParameter("task_id", this.getId())
+        .addParameter("category_id", category.getId())
+        .executeUpdate();
+    }
+  }
+
+  public List<Category> getCategories() {
+    try(Connection con = DB.sql2o.open()) {
+      String joinQuery = "SELECT category_id FROM tasks_categories WHERE task_id = :task_id";
+      List<Integer> categoryIds = con.createQuery(joinQuery)
+        .addParameter("task_id", this.getId())
+        .executeAndFetch(Integer.class);
+
+      List<Category> categories = new ArrayList<Category>();
+
+      for (Integer categoryId : categoryIds) {
+        String taskQuery = "SELECT * FROM categories WHERE id = :categoryId";
+        Category category = con.createQuery(taskQuery)
+          .addParameter("categoryId", categoryId)
+          .executeAndFetchFirst(Category.class);
+        categories.add(category);
+      }
+      return categories;
+    }
+  }
+
+
 
 }
